33e64f87ce5cb5dc01322f357f77b8b8b6105171
panzp <panzp@student.unimelb.edu.au>
2024-08-27 22:00:53 +1000

    fixed autofilled bug and complete basic function of login page

---
+++ togglePassword.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#password');
+
+    togglePassword.addEventListener('click', function () {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+    }
+)},)

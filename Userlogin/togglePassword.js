1a55afc3d1cc5058cd6da079785daf328ba87a23
panzp <panzp@student.unimelb.edu.au>
2024-09-12 20:57:44 +1000

    finished the second web page of account creating

--- Userlogin/togglePassword.js
+++ Userlogin/togglePassword.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // 密码框逻辑
     const togglePassword = document.querySelector('#togglePassword');
     const password = document.querySelector('#password');
 
@@ -12,7 +13,25 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             togglePassword.classList.add('icon-view_off'); 
         }
-        
+
         console.log(togglePassword.classList);
+    });
+
+    // 确认密码框逻辑
+    const toggleConfirmPassword = document.querySelector('#toggleConfirmPassword');
+    const confirmPassword = document.querySelector('#confirmPassword');
+
+    toggleConfirmPassword.addEventListener('click', function () {
+        const isConfirmPasswordVisible = confirmPassword.getAttribute('type') === 'text';
+        confirmPassword.setAttribute('type', isConfirmPasswordVisible ? 'password' : 'text');
+        toggleConfirmPassword.classList.remove('icon-view_off', 'icon-view-2');
+
+        if (isConfirmPasswordVisible) {
+            toggleConfirmPassword.classList.add('icon-view-2');
+        } else {
+            toggleConfirmPassword.classList.add('icon-view_off'); 
+        }
+
+        console.log(toggleConfirmPassword.classList);
     });
 });
